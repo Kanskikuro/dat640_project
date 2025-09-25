8b97e2758037f8a99cb6cbbc836d428a235c6708
Kanskikuro <william.vo@live.no>
2025-09-25 16:24:41 +0200

    feat(R2 and R3) Implementing some features for db logic. This includes, R2.1,2,5 and R3.1,2

--- musiccrs/config.py
+++ musiccrs/config.py
@@ -2,6 +2,11 @@ OLLAMA_HOST = "https://ollama.ux.uis.no"
 OLLAMA_MODEL = "llama3.3:70b"
 OLLAMA_API_KEY = "SET YOUR API KEY HERE"
 
-DB_PATH = "data/spotify_db.json" 
+DB_FOLDER = "data"
+DB_PATH = "data/spotify.db" 
+MPD_DATA = "mpd.v1\data"
+
+
+from dialoguekit.core.intent import Intent
 
 _INTENT_OPTIONS = Intent("OPTIONS")
